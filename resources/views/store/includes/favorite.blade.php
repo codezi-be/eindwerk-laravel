b2179506b88cea3396a0358f757369f7e796e197
codezi-be <verbovenziggy@gmail.com>
2026-03-20 14:28:26 +0100

    klaar met de stap van favoritesController

--- resources/views/store/includes/favorite.blade.php
+++ resources/views/store/includes/favorite.blade.php
@@ -1,9 +1,13 @@
 <a href="{{ route('favorites.toggle', $product) }}" class="group text-2xl bg-white px-4 py-1">
-    {{-- Als not favorite --}}
-    <i class="group-hover:hidden fa-solid fa-heart"></i>
-    <i class="hidden group-hover:inline fa-solid fa-heart"></i>
-
-    {{-- Als favorite --}}
-    {{-- <i class="group-hover:hidden text-red-500 fa-solid fa-heart"></i>
-    <i class="hidden group-hover:inline text-red-500 fa-solid fa-heart"></i> --}}
+    @auth
+    @if (!Auth::user()->favorites()->where('id', $product->id)->count())
+        {{-- Als not favorite --}}
+        <i class="group-hover:hidden fa-solid fa-heart"></i>
+        <i class="hidden group-hover:inline fa-solid fa-heart"></i>
+    @else
+        {{-- Als favorite --}}
+        <i class="group-hover:hidden text-red-500 fa-solid fa-heart"></i>
+        <i class="hidden group-hover:inline text-red-500 fa-solid fa-heart"></i>
+    @endif
+    @endauth
 </a>
